e686913c5726e7ca5d838451dab9c94a9dd23660
Emmanuel Castillo <>
2021-05-02 21:33:53 +0000

    BMI Calculator finish.

--- main.py
+++ main.py
@@ -4,14 +4,21 @@ weight = input("enter your weight in kg: ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-m = int(height)
-kg = float(weight)
-elevate = (kg ** 2)
-operation = (m / elevate)
-convertion = str(operation)
-a = str(convertion[0])
-b = str(convertion[1])
-print(a + b)
+#Teacher's way. Much better than me, but a try.
+bmi = int(weight) / float(height) ** 2
+bmi_as_int = int(bmi)
+print(bmi_as_int)
+
+
+#my way
+# m = int(height)
+# kg = float(weight)
+# elevate = (kg ** 2)
+# operation = (m / elevate)
+# convertion = str(operation)
+# a = str(convertion[0])
+# b = str(convertion[1])
+# print(a + b)
 
 
 
